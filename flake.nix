d5b56f5c8e4a90e0235a81611094528ab7559333
Ruoshan Huang <ruoshan.huang@gmail.com>
2024-03-10 00:15:53 +0800

    Nix fmt and fix GOPROXY

--- flake.nix
+++ flake.nix
@@ -8,38 +8,47 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem ( system:
-      let
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
           pkgs = nixpkgs.legacyPackages.${system};
-      in rec {
-        packages.mipsle = (pkgs.buildGoModule {
-          pname = "http2-proxy-redir";
-          version = "dev";
-          src = ./.;
-          vendorHash = "sha256-Qdyz6YdAfCwOHy2g/EzjsJGg2M41fbE6M8ydaLcgc58=";
-        }).overrideAttrs ( old: old // { GOPROXY="direct"; GOOS="linux"; GOARCH = "mipsle"; CGO_ENABLED = 0; doCheck = false; } );
+        in
+        rec {
+          packages.mipsle = (pkgs.buildGoModule {
+            pname = "http2-proxy-redir";
+            version = "dev";
+            src = ./.;
+            vendorHash = "sha256-Qdyz6YdAfCwOHy2g/EzjsJGg2M41fbE6M8ydaLcgc58=";
+            overrideModAttrs = (old: { GOPROXY = "direct"; });
+          }).overrideAttrs (old: { GOOS = "linux"; GOARCH = "mipsle"; CGO_ENABLED = 0; doCheck = false; });
 
-        packages.arm64 = (pkgs.buildGoModule {
-          pname = "http2-proxy-redir";
-          version = "dev";
-          src = ./.;
-          vendorHash = "sha256-Qdyz6YdAfCwOHy2g/EzjsJGg2M41fbE6M8ydaLcgc58=";
-        }).overrideAttrs ( old: old // { GOPROXY="direct"; GOOS="linux"; GOARCH = "arm64"; CGO_ENABLED = 0; doCheck = false; } );
+          packages.arm64 = (pkgs.buildGoModule {
+            pname = "http2-proxy-redir";
+            version = "dev";
+            src = ./.;
+            vendorHash = "sha256-Qdyz6YdAfCwOHy2g/EzjsJGg2M41fbE6M8ydaLcgc58=";
+            overrideModAttrs = (old: { GOPROXY = "direct"; });
+          }).overrideAttrs (old: { GOOS = "linux"; GOARCH = "arm64"; CGO_ENABLED = 0; doCheck = false; });
 
-        packages.amd64 = (pkgs.buildGoModule {
-          pname = "http2-proxy-redir";
-          version = "dev";
-          src = ./.;
-          vendorHash = "sha256-Qdyz6YdAfCwOHy2g/EzjsJGg2M41fbE6M8ydaLcgc58=";
-        }).overrideAttrs ( old: old // { GOPROXY="direct"; GOOS="linux"; GOARCH = "amd64"; CGO_ENABLED = 0; doCheck = false; } );
+          packages.amd64 = (pkgs.buildGoModule {
+            pname = "http2-proxy-redir";
+            version = "dev";
+            src = ./.;
+            vendorHash = "sha256-Qdyz6YdAfCwOHy2g/EzjsJGg2M41fbE6M8ydaLcgc58=";
+            overrideModAttrs = (old: { GOPROXY = "direct"; });
+          }).overrideAttrs (old: { GOOS = "linux"; GOARCH = "amd64"; CGO_ENABLED = 0; doCheck = false; });
 
-        defaultPackage = packages.mipsle;
+          defaultPackage = packages.mipsle;
 
-        devShells.default = pkgs.mkShell {
-          inputsFrom = [ packages.mipsle ];
-          shellHook = ''
+          devShells.default = pkgs.mkShell {
+            inputsFrom = [ packages.mipsle ];
+            shellHook = ''
           '';
-        };
-      }
-    );
+          };
+        }
+      ) // {
+      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
+      formatter.aarch64-darwin = nixpkgs.legacyPackages.aarch64-darwin.nixpkgs-fmt;
+      formatter.x86_64-darwin = nixpkgs.legacyPackages.x86_64-darwin.nixpkgs-fmt;
+    };
 }
